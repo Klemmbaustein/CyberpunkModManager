19974bd0aad9bf89af21e190df49e327301c0357
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2024-05-19 21:46:19 +0200

    update setup script

--- Setup.ps1
+++ Setup.ps1
@@ -21,13 +21,25 @@ cmake -S . -B Build/
 cd Build/
 msbuild.exe webp.vcxproj /p:Configuration=Release /p:Platform=x64
 
-# Dependencies/7zip/CPP/7zip
-cd ../../7zip/CPP/7zip
-
 # 7zip is insane.
-nmake
-mkdir ../../../../App/app/bin -f
-cp Bundles/Alone/o/7za.exe ../../../../App/app/bin/
+
+if ($args[0] -eq "CI_BUILD")
+{
+	cd ../../..
+	Invoke-WebRequest https://www.7-zip.org/a/7z2405-extra.7z -OutFile 7zip.7z
+	7z x -y 7zip.7z -o7zipFiles/
+	cp 7ZipFiles/7za.exe App/app/bin/
+	rm -r -fo 7zipFiles
+	rm 7zip.7z
+}
+else
+{
+	# Dependencies/7zip/CPP/7zip
+	cd ../../7zip/CPP/7zip
+	nmake
+	mkdir ../../../../App/app/bin -f
+	cp Bundles/Alone/o/7za.exe ../../../../App/app/bin/
+	cd ../../../..
+}
 
 # ./
-cd ../../../..
